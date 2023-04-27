bf7307148bf94a9dae305514db7e66a08cc36f98
Wisdom Ochiche (Juicio) <wisdomochiche@gmail.com>
2023-04-27 23:01:04 +0100

    initial commit

---
+++ 0-lockboxes.py
@@ -0,0 +1,19 @@
+def canUnlockAll(boxes):
+    #initialize a list of unlocked boxes
+    unlocked = [False] * len(boxes)
+    #set the first box oprn
+    unlocked[0] = True
+    #iterate over the boxes
+    for index, box in enumerate(boxes):
+        #check if the box is unlocked
+        if unlocked[index]:
+            #get the keys in the box
+            for index, key in enumerate(box):
+                #set the box with a found key to open
+                if key < len(unlocked): 
+                    unlocked[key] = True
+                    #get the keys at the box that has been opened
+                    # set the boxes with the keys to be open
+                    for i in boxes[key]:
+                        unlocked[i] = True
+    return all(unlocked)
